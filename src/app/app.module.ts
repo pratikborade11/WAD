4b41fdf80388edcde81619828cc6234dc25569fa
pratikborade11 <128473206+pratikborade11@users.noreply.github.com>
2024-02-13 20:59:14 +0530

    Files updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { HomeComponent } from './component/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
